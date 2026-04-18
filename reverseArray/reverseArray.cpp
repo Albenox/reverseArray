b7ac7db9837a61b3fa939e8427a898675ee5693f
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2026-04-17 22:33:02 -0400

    Completed the reverse array so that the original array can be reversed and printed, now needing to clean up comments and work on delete[] to clean the arrays after use.

--- reverseArray/reverseArray.cpp
+++ reverseArray/reverseArray.cpp
@@ -6,9 +6,10 @@
 5. Deallocate the dynamically allocated array using delete[].
 */
 
-//int* ReverseArray(const int arr[], int size);
+int* ReverseArray(const int arr[], int size);
 void print(const int arr[], int size);
 
+//Libraries and namespace std for convinience
 #include <iostream>
 using namespace std;
 
@@ -19,29 +20,51 @@ int main()
 	cout << "Enter the size of the array: ";
 	cin >> capacity;
 
-	//Declares(int*) a pointer called arr to an int array of integers that has a dynamic capacity of the value entered by the user
+	//Declares(int*) a pointer called arr that is set to an int array of integers that has a dynamic capacity of the value entered by the user
 	//This makes the array reusable after delete[], and so the capacity can be dynamically set based on the user's input
 	int* arr = new int[capacity];
 
-	//
+	//A for loop that runs through the array until the capacity is reached by prompting the user for an integer
 	for (int i = 0; i < capacity; i++)
 	{
-		cout << "Enter element " << i + 1 << ": ";
+		cout << "Enter integer " << i + 1 << ": ";
 		cin >> arr[i];
 	}
-	cout << endl;
+	
+	//We are returning reversed, which is just the address to the array new int[size] made in the function, which technically means we are not sending the array back to main, but just the address so it can be found in the memory
+	int* reversedArr = ReverseArray(arr, capacity);
+	
+	//Prints both the original and reversed arrays
+	cout << endl << "The original array is: ";
 	print(arr, capacity);
+	cout << endl << "The reversed array is: ";
+	print(reversedArr, capacity);
+
+	return 0;
 }
 
 //ReverseArray dynamically allocates an array, copies the contents of the original array in reverse order, and returns a pointer to the new array.
-/*int* ReverseArray(const int arr[], int size)
+int* ReverseArray(const int arr[], int size)
 {
-	cout << "reverseArray";
-}*/
+	//Declares(int*) a pointer called arr that is set to an int array of integers that has a dynamic capacity of the value entered by the user
+	//This makes the array reusable after delete[], and so the capacity can be dynamically set based on the user's input
+	int* reversed = new int[size];
+
+	//For loop that runs through the array a number of times equal to the size of the array
+	for (int i = 0; i < size; i++)
+	{
+		//Sets the value of the current index of the reversed array to the end of the original array, with i increasing each loop, causing the loop to move backwards through the original array
+		reversed[i] = arr[size - 1 - i];
+	}
+
+	//Returns the ADDRESS of the reversed array to bypass C++ not normally allowing arrays to be returned between functions
+	return reversed;
+}
 
 //print displays the contents of an array.
 void print(const int arr[], int size)
 {
+	cout << endl;
 	for (int i = 0; i < size; i++) {
 		cout << arr[i] << " ";
 	}
